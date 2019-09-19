705b4aabe5e14a8ad0c479f05286d16eded9ea8e
lhy0504 <hylee09.1173@gmail.com>
2019-09-19 20:26:14 +0800

    initial

--- 1_Lan_C/average.c
+++ 1_Lan_C/average.c
@@ -1,9 +1,24 @@
-/*The file take 10 integers from input*
+/*The file take 10 integers from input
   and then promote the average of the 10 inputs
- * /
+ */
+#include <stdio.h>
+#include "average.h"
 
+
+float avg(){
+  printf("Enter 10 integers:");
+  int sum=0;
+  int cur;
+  for(int i=0;i<10;i++){
+    scanf("%d",&cur);
+    sum+=cur;
+  }
+  return (float)sum/10;
+};
 
 int main ()
 {
-	return 0;
+  float average = avg();
+  printf("Average : %f",average);
+  return 0;
 }
---
+++ 1_Lan_C/average.h
@@ -0,0 +1,13 @@
+float avg();
+
+/*Name: Lee Hong Yau, Henry*/
+
+enum myEnum{
+    ERR,SUCCESS,DONE
+};
+
+struct {
+    enum myEnum a;
+    int b;
+    float *d;
+}myStruct;
