a577674746ba5633f4c1691350faa6f1a38b30b7
Anchit Mishra <anchitmishra1@gmail.com>
2019-09-19 17:53:41 +0800

    Added the files for Assignment 1

--- 1_Lan_C/average.c
+++ 1_Lan_C/average.c
@@ -1,9 +1,21 @@
-/*The file take 10 integers from input*
-  and then promote the average of the 10 inputs
- * /
+#include <stdio.h>
+#include "assignment_1.h"
 
+float average()	{
+	printf("Enter 10 integers: ");
+	int i = 0;
+	int integers[10];
+	int average = 0;
+	for (i = 0; i < 10; i++)	{
+		scanf(" %d", &integers[i]);
+		average += integers[i];
+		}
+	average /= 10;
+	return average;
+	}
 
-int main ()
-{
+int main()	{
+	int avg = average();
+	printf("%d\n", avg);
 	return 0;
-}
+	}
---
+++ 1_Lan_C/average.h
@@ -0,0 +1,9 @@
+float average();
+//Anchit Mishra
+enum mission_status{ERR, SUCCESS, DONE};
+
+struct xyz	{
+	int a;
+	float * c;
+	enum mission_status mission_1;
+	};
