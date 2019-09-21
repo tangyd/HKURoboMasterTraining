4056b87ece3d51ad1302f85f3dec77a48ec0512a
Fuyen999 <55078561+Fuyen999@users.noreply.github.com>
2019-09-21 15:47:25 +0800

    Create assignment1.c
    .c file ~ calculate average of 10 numbers

---
+++ assignment1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float average(){
+
+    int a, b, c, d, e, f, g, h, i, j;
+
+    printf("Enter 10 integers: ");
+    scanf("%d %d %d %d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j);
+
+    float sum = a+b+c+d+e+f+g+h+i+j;
+
+    return sum/10;
+}
+
+int main()
+{
+    printf("%f", average());
+
+    return 0;
+}
