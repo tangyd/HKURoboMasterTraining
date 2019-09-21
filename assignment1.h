53a3764d9ef84dfb0f7ca452c7ff0d62710133c4
Fuyen999 <55078561+Fuyen999@users.noreply.github.com>
2019-09-21 16:39:27 +0800

    .h file

---
+++ assignment1.h
@@ -0,0 +1,26 @@
+#ifndef USELESSHEADER_H_INCLUDED
+#define USELESSHEADER_H_INCLUDED
+
+int uselessFunction(){
+    /*Name: Yew Fu Yen
+    Why declare a function here??? I dun understand the question lol*/
+    return 0;
+}
+
+enum uselessEnum
+{
+    ERR, SUCCESS, DONE
+} ;
+
+float num;
+
+struct uselessStruct
+{
+    enum uselessEnum eF0;
+    int lol;
+    int*addr_num;
+
+};
+
+
+#endif // USELESSHEADER_H_INCLUDED
