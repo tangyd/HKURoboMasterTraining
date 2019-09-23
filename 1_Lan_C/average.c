443d816b4a111083a7c6f5a6aef2bfff7dbdb430
jiarui <jiaruiz@connect.hku.hk>
2019-09-23 15:54:02 +0800

    assignment01

--- 1_Lan_C/average.c
+++ 1_Lan_C/average.c
@@ -1,9 +1,20 @@
-/*The file take 10 integers from input*
-  and then promote the average of the 10 inputs
- * /
+//*The file take 10 integers from input*
+ // and then promote the average of the 10 inputs
+ //* /
 
+
+#include <stdio.h>
 
 int main ()
-{
+{   float average,x;
+    int i;
+    i=0;
+    for (i = 0; i<10; i++)
+	{   scanf("%f",&x);
+	    average += x/10;
+	    
+}
+    printf("%f",average);
 	return 0;
 }
+
