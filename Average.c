ab09b0b488ee330328c5fce7e0ed05799f0d2edc
Justin_Yim_Yim <55522706+SleepyDicker@users.noreply.github.com>
2019-09-23 14:05:49 +0800

    [BREIF INTRO]

---
+++ Average.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "IDKWhyThisIsNecessary.h"
+
+float average(float temp)
+{
+	temp = temp/10;
+	printf("Average of your values:%f",temp);
+	return 0;
+}
+
+int main()
+{
+	int temp1, temp, i;
+	temp1=0;
+	temp=0
+	for(i=0; i<10; i++){
+		scanf("Enter 10 number:%f",&temp);
+		temp1+=temp
+		printf("number remain to be entered:%d",(9-i));
+}
+	average(temp1);
+	return 0;
+}
---
+++ IDKWhyThisIsNecessary.h
@@ -0,0 +1,9 @@
+enum XXX{
+	cool, Not_Cool,really_NOt_Cool
+}CocaCola;
+
+struct Pepsi{
+	int volume;
+	char Name;
+	int CocaCola;
+};
