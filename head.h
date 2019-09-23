39c0e21bded3a982142f47c28d5008d2f73ba858
AliceHexin <hexin99@hku.hk>
2019-09-23 22:37:12 +0800

    homework_headfile

---
+++ head.h
@@ -0,0 +1,19 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+myfunction(); 
+/*He Xin*/ 
+
+enum enum1
+{
+	ERR=0,SUCCESS=1,DONE=2
+};
+
+struct struct1
+{
+	enum enum1 ERR;
+	int num1;
+	float* addr_num0;
+};
+ 
+#endif
