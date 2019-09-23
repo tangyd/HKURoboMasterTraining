cdcd7d288da3477940caaba46263b33bf282236c
Michael WU <vusmic@Macintosh.local>
2019-09-23 21:13:56 +0800

    assignment1 commit

---
+++ assign1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"assign1.h"
+
+float takeaverage()
+{
+	float sum = 0;
+	int n;
+	for (int i = 0; i<10; i++){
+		scanf("%d", &n);
+		sum += n;
+	}
+	return sum/10;
+}
+
+int main()
+{
+	printf("%f\n", takeaverage());
+	return 0;
+}
---
+++ assign1.h
@@ -0,0 +1,13 @@
+//WU Gaochen
+float takeaverage();
+
+enum myEnum{
+	ERR,SUCCESS,DONE
+};
+
+struct myStruct
+{
+	enum myEnum e;
+	int i;
+	float * f;
+};
