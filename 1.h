de47a4922fea6272780dcabaa23fa253667af470
tangyd <55542595+tangyd@users.noreply.github.com>
2019-09-24 23:12:56 +0800

    1.h

---
+++ 1.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+Average();
+    /* Tang Yudan*/
+enum Myenum
+{
+    ERR=0, SUCCESS=1, DONE=2,
+};
+struct Mystruct
+{
+    enum Myenum ERR;
+    int iInt;
+    float* pFloat;
+};
